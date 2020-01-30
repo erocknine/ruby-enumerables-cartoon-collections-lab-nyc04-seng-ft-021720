6366805c5371c16204a8542ded8ebd5f5df3f55f
Sir Eric <echeungsk@gmail.com>
2020-01-30 02:11:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(elements)
-  elements.map.each {|x| puts "#{x.capitalize}!"}
+  elements.map {|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
