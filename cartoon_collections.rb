84c861382fe7312d0c44996055fe0eecbb6871ce
Sir Eric <echeungsk@gmail.com>
2020-01-30 02:19:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(elements)
   elements.map {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls
-  
+def long_planeteer_calls(array)
+  array.reduce {|x| 
 end
 
 def find_the_cheese# code an argument here
