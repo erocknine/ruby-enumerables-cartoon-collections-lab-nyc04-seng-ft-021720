c940b0df508696e4ccedd0db6898df219a82836c
Sir Eric <echeungsk@gmail.com>
2020-01-30 02:33:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(array)
   array.any? {|x| x.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
